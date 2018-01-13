c76ff90b0b8180ca6fcfcdbd5cc7e0905beb629c
段泽全 <4884280@qq.com>
2018-01-13 22:02:02 +0800

    DifferRepair

---
+++ libsdifferrepair/src/main/java/com/aliletter/differrepair/DifferStatus.java
@@ -0,0 +1,16 @@
+package com.aliletter.differrepair;
+
+/**
+ * Author: mr-absurd
+ * Github: http://github.com/mr-absurd
+ * Data: 2017/8/18.
+ */
+
+public enum DifferStatus {
+
+    OLDAPK_NON_EXIST,
+    PATCH_NON_EXIST,
+    NEWAPK_EXIST,
+    SUCCESS
+
+}
