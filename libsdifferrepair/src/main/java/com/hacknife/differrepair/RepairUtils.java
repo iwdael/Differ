49f06334eadbb3a79b25e993ee759386d6ad90cb
Hacknife <@dd410520>
2018-10-10 14:07:48 +0800

    适配api24

--- libsdifferrepair/src/main/java/com/hacknife/differrepair/RepairUtils.java
+++ libsdifferrepair/src/main/java/com/hacknife/differrepair/RepairUtils.java
@@ -3,13 +3,15 @@ package com.hacknife.differrepair;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
 
 import java.io.File;
 
 
 /**
- * Author: mr-absurd
- * Github: http://github.com/mr-absurd
+ * Author: hacknife
+ * Github: http://github.com/hacknife
  * Data: 2017/8/18.
  */
 
@@ -26,6 +28,14 @@ public class RepairUtils {
         Uri uri = Uri.parse("package:" + packageName);
         Intent intent = new Intent(Intent.ACTION_DELETE, uri);
         context.startActivity(intent);
+    }
+
+    public static Uri uriFromFile(Context context,File file){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            return FileProvider.getUriForFile(context,"com.hacknife.differrepair.FileProvider",file);
+        }else{
+            return  Uri.fromFile(file);
+        }
     }
 
 }
