a30327a4ad7a90b1dab52833d1169f0fbef09f87
Hacknife <@dd410520>
2018-10-10 14:44:59 +0800

    适配api24

--- libsdifferrepair/src/main/java/com/hacknife/differrepair/RepairUtils.java
+++ libsdifferrepair/src/main/java/com/hacknife/differrepair/RepairUtils.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.content.FileProvider;
 
 import java.io.File;
@@ -20,7 +21,7 @@ public class RepairUtils {
     public static void install(Context context, File apkFile) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setDataAndType(uriFromFile(context,apkFile), "application/vnd.android.package-archive");
+        intent.setDataAndType(uriFromFile(context, apkFile), "application/vnd.android.package-archive");
         context.startActivity(intent);
     }
 
@@ -30,12 +31,29 @@ public class RepairUtils {
         context.startActivity(intent);
     }
 
-    public static Uri uriFromFile(Context context,File file){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
-            return FileProvider.getUriForFile(context,context.getPackageName()+".FileProvider",file);
-        }else{
-            return  Uri.fromFile(file);
+    public static Uri uriFromFile(Context context, File file) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return FileProvider.getUriForFile(context, context.getPackageName() + ".FileProvider", file);
+        } else {
+            return Uri.fromFile(file);
         }
     }
 
+
+    public static boolean haveUnknownAppInstallPermission(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return context.getPackageManager().canRequestPackageInstalls();
+        } else {
+            return true;
+        }
+    }
+
+    public static void openUnknownAppInstallPermission(Context context) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return;
+        }
+        Uri uri = Uri.parse("package:" + context.getPackageName());
+        Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, uri);
+        context.startActivity(intent);
+    }
 }
